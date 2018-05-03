65ade20875dc95778430d5929db9108c32b3be23
Rafael Mateus Da Silva <rafaelmateus@brq.com>
2018-05-03 15:40:59 -0300

    Adicionado entity de endereco e modificado mapeado entidade no persistence.xml

---
+++ src/main/java/entity/Endereco.java
@@ -0,0 +1,123 @@
+package entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+/**
+ * @author Rafael Mateus Classe de entidade que representara a model e também as
+ *         tabelas para persistencia por JPA de Enderecos 03 de maio de 2018
+ */
+
+@Entity
+@Table(name = "Endereco")
+public class Endereco implements Serializable {
+
+	private static final long serialVersionUID = 4683554548002572431L;
+
+	@Id
+	@Column(name = "ID_ENDERECO", unique = true, nullable = false)
+	private Integer idEndereco;
+	
+	@Column(name = "ID_USUARIO")
+	private Integer idUsuario;
+	
+	@Column(name = "ESTADO")
+	private String estado;
+	
+	@Column(name = "CEP")
+	private String cep;
+	
+	@Column(name = "CIDADE")
+	private String cidade;
+	
+	@Column(name = "BAIRRO")
+	private String bairro;
+	
+	@Column(name = "RUA")
+	private String rua;
+	
+	@Column(name = "NUMERO")
+	private String numero;
+	
+	@Column(name = "COMPLEMENTO")
+	private String complemento;
+	
+	public Endereco(){}
+	
+	public Integer getIdEndereco() {
+		return idEndereco;
+	}
+
+	public void setIdEndereco(Integer idEndereco) {
+		this.idEndereco = idEndereco;
+	}
+
+	public Integer getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Integer idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+}
