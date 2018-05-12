42a0760f3b1d3c94f0e5847036dd12637f3ffc3f
Rafael Mateus <Rafael.Mateus.Silva@hotmail.com>
2018-05-12 12:40:51 -0300

    Corrigido scripts, apagado tabelas e melhorias

--- src/scriptDb.sql
+++ src/scriptDb.sql
@@ -18,21 +18,10 @@ SET SERVEROUTPUT ON
 BEGIN
      Dbms_Output.Put_Line(Systimestamp);
 END;
-
-CREATE TABLE ERROR(
-	ERRORR VARCHAR(128) NOT NULL,
-	ERROOR VARCHAR(128) NOT NULL,
-	ERRROR VARCHAR(128) NOT NULL,
-	EERROR VARCHAR(128) NOT NULL,
-	ERRORRR VARCHAR(128) NOT NULL,
-	ERRORE VARCHAR(128) NOT NULL,
-	ERRORRE VARCHAR(128) NOT NULL,
-	ERRORRRRE VARCHAR(128) NOT NULL
-	ERRORER VARCHAR(64));
-
+/
 
 CREATE TABLE Usuarios (
-    id_usuario INTEGER NOT NULL CONSTRAINT USUARIO_PK PRIMARY KEY,
+    Id_usuario INTEGER NOT NULL CONSTRAINT USUARIO_PK PRIMARY KEY,
     permissionLevel INTEGER NOT NULL,
     isTecnico INTEGER NOT NULL,
 	Nome VARCHAR(128) NOT NULL,
@@ -47,7 +36,7 @@ CREATE TABLE Usuarios (
     rua VARCHAR(64),
     complemento VARCHAR(64));
 	
-CREATE TABLE Endereco2 (
+CREATE TABLE Enderecos_Secundarios (
 	ID_Endereco INTEGER NOT NULL CONSTRAINT ENDERECO_PK PRIMARY KEY,
 	ID_Usuario INTEGER NOT NULL,
 	Estado VARCHAR2(64),
@@ -65,50 +54,30 @@ CREATE TABLE Categorias_Servicos (
 
 CREATE TABLE Servicos (
     ID_Servico INTEGER NOT NULL CONSTRAINT SERVICO_PK PRIMARY KEY,
-    ID_Usuario INTEGER NOT NULL,
+    ID_Tecnico INTEGER NOT NULL,
     ID_Categoria INTEGER NOT NULL,
     Descricao VARCHAR2(64),
     Preco NUMBER(8,2),   
-    CONSTRAINT FK_Usuario FOREIGN KEY(ID_Usuario) REFERENCES Usuarios(id_usuario),
+    CONSTRAINT FK_Tecnico FOREIGN KEY(ID_Tecnico) REFERENCES Usuarios(id_usuario),
     CONSTRAINT FK_Categoria FOREIGN KEY(ID_Categoria) REFERENCES Categorias_Servicos(ID_CATEGORIA));
-	
-CREATE TABLE Avaliacoes (
-	ID_Avaliacao INTEGER NOT NULL CONSTRAINT AVALIACAO_PK PRIMARY KEY,
-	ID_Servico INTEGER NOT NULL,
-	Nota INTEGER NOT NULL,
-    CONSTRAINT FK_SERVICO FOREIGN KEY(ID_Servico) REFERENCES SERVICOS(ID_Servico));
-    
+	    
 CREATE TABLE Pedidos (
 	ID_Pedido INTEGER NOT NULL CONSTRAINT PEDIDO_PK PRIMARY KEY,
 	ID_Usuario_Solicitante INTEGER NOT NULL,
 	ID_Tecnico INTEGER NOT NULL,
 	ID_Servico INTEGER NOT NULL,
-	ID_Nota INTEGER NOT NULL,
 	Data_Solicitacao timestamp,
 	Data_Finalizacao timestamp,
+	Nota INTEGER,
     CONSTRAINT FK_PEDIDO_SERVICO FOREIGN KEY(ID_Servico) REFERENCES SERVICOS(ID_Servico),
 	CONSTRAINT FK_USUARIO_SOLICITANTE FOREIGN KEY(ID_Usuario_Solicitante) REFERENCES USUARIOS(ID_USUARIO),
-	CONSTRAINT FK_TECNICO FOREIGN KEY(ID_Tecnico) REFERENCES USUARIOS(ID_USUARIO));
-	
-CREATE TABLE Pedidos_Finalizados (
-	ID_Pedido_Finalizado INTEGER NOT NULL CONSTRAINT SERVICO_FINALIZADO_PK PRIMARY KEY,
-	ID_Servico INTEGER NOT NULL,
-    ID_Pedido INTEGER NOT NULL,
-	ID_Categoria INTEGER NOT NULL,
-	ID_NOTA INTEGER NOT NULL,
-	DataFinalizacao TIMESTAMP,
-	ID_USUARIO_CONTRATANTE INTEGER NOT NULL,
-	ID_TECNICO_FORNECEDOR INTEGER NOT NULL,
-    CONSTRAINT FK_Servico_Finalizado FOREIGN KEY(ID_Servico) REFERENCES SERVICOS(ID_Servico),
-	CONSTRAINT FK_NOTA FOREIGN KEY(ID_NOTA) REFERENCES AVALIACOES(ID_AVALIACAO),
-    CONSTRAINT FK_ID_PEDIDO FOREIGN KEY(ID_Pedido) REFERENCES PEDIDOS(ID_Pedido),
-    CONSTRAINT FK_Pedido_Finalizada FOREIGN KEY(ID_Categoria) REFERENCES Categorias_Servicos(ID_CATEGORIA));
-   
-CREATE SEQUENCE USUARIO_SEQ
+	CONSTRAINT FK_PEDIDO_TECNICO FOREIGN KEY(ID_Tecnico) REFERENCES USUARIOS(ID_USUARIO));
+	   
+CREATE SEQUENCE USUARIOS_SEQ
 START WITH 1
 INCREMENT BY 1;
 
-CREATE SEQUENCE SERVICO_SEQ
+CREATE SEQUENCE SERVICOS_SEQ
 START WITH 1
 INCREMENT BY 1;
 
@@ -120,15 +89,17 @@ CREATE SEQUENCE PEDIDOS_SEQ
 START WITH 1
 INCREMENT BY 1;
 
-CREATE SEQUENCE SERVICOS_FINALIZADOS_SEQ
-START WITH 1
-INCREMENT BY 1;
-       
+--DROP SEQUENCE USUARIOS_SEQ
+--DROP SEQUENCE SERVICOS_SEQ
+--DROP SEQUENCE CATEGORIAS_SEQ
+--DROP SEQUENCE PEDIDOS_SEQ
+      
 -------------------------------MASSAS PARA TESTES-----------------------------------------------------------------
 INSERT INTO Categorias_Servicos(id_categoria, descricao) VALUES (CATEGORIAS_SEQ.NEXTVAL,'Chaveiro');
-INSERT INTO Categorias_Servicos(id_categoria, descricao) VALUES (CATEGORIAS_SEQ.NEXTVAL,'Detetizador');
-INSERT INTO Categorias_Servicos(id_categoria, descricao) VALUES (CATEGORIAS_SEQ.NEXTVAL,'Desentupidor');
+INSERT INTO Categorias_Servicos(id_categoria, descricao) VALUES (CATEGORIAS_SEQ.NEXTVAL,'Desenvolvedor');
 INSERT INTO Categorias_Servicos(id_categoria, descricao) VALUES (CATEGORIAS_SEQ.NEXTVAL,'Eletricista');
+INSERT INTO Categorias_Servicos(id_categoria, descricao) VALUES (CATEGORIAS_SEQ.NEXTVAL,'Desentupidor');
+INSERT INTO Categorias_Servicos(id_categoria, descricao) VALUES (CATEGORIAS_SEQ.NEXTVAL,'Detetizador');
 INSERT INTO Categorias_Servicos(id_categoria, descricao) VALUES (CATEGORIAS_SEQ.NEXTVAL,'Encanador');
 INSERT INTO Categorias_Servicos(id_categoria, descricao) VALUES (CATEGORIAS_SEQ.NEXTVAL,'Jardineiro');
 INSERT INTO Categorias_Servicos(id_categoria, descricao) VALUES (CATEGORIAS_SEQ.NEXTVAL,'Pedreiro');
@@ -137,58 +108,68 @@ INSERT INTO Categorias_Servicos(id_categoria, descricao) VALUES (CATEGORIAS_SEQ.
 INSERT INTO Categorias_Servicos(id_categoria, descricao) VALUES (CATEGORIAS_SEQ.NEXTVAL,'Terraplanista');
 
 insert into usuarios(ID_USUARIO, PERMISSIONLEVEL, ISTECNICO, CPF, EMAIL, SENHA, NOME, TELEFONE, ESTADO, CIDADE, CEP, BAIRRO, RUA,
-COMPLEMENTO) VALUES(1,1,1,'00000000000', 'rafael@hotmail.com', '123456', 'Rafael Mateus', '123456789', 'Parana', 'Curitiba', 'TESTE', 'TESTE', 'TESTE','TESTE');
+COMPLEMENTO) VALUES(USUARIOS_SEQ.NEXTVAL,1,1,'00000000000', 'rafael@hotmail.com', '123456', 'Rafael Mateus', '123456789', 'TESTSTATE', 'TESTCITY', 'TESTE', 'TESTE', 'TESTE','TESTE');
 
 insert into usuarios(ID_USUARIO, PERMISSIONLEVEL, ISTECNICO, CPF, EMAIL, SENHA, NOME, TELEFONE, ESTADO, CIDADE, CEP, BAIRRO, RUA,
-COMPLEMENTO) VALUES(2,1,1,'00000000000', 'joao@hotmail.com', '56465464', 'Joao Pedro', '123456789', 'Parana', 'Guaratuba', 'TESTE', 'TESTE', 'TESTE','TESTE');
+COMPLEMENTO) VALUES(USUARIOS_SEQ.NEXTVAL,1,1,'00000000000', 'joao@hotmail.com', '56465464', 'Joao Pedro', '123456789', 'TESTSTATE', 'TESTCITY', 'TESTE', 'TESTE', 'TESTE','TESTE');
 
 insert into usuarios(ID_USUARIO, PERMISSIONLEVEL, ISTECNICO, CPF, EMAIL, SENHA, NOME, TELEFONE, ESTADO, CIDADE, CEP, BAIRRO, RUA,
-COMPLEMENTO) VALUES(3,1,1,'00000000000', 'thanos@hotmail.com', '123456', 'Thanos', '123456789', 'Parana', 'Matinhos', 'TESTE', 'TESTE', 'TESTE','TESTE');
+COMPLEMENTO) VALUES(USUARIOS_SEQ.NEXTVAL,1,1,'00000000000', 'thanos@hotmail.com', '123456', 'Thanos', '123456789', 'Parana', 'Anywhere', 'TESTE', 'TESTE', 'TESTE','TESTE');
 
 insert into usuarios(ID_USUARIO, PERMISSIONLEVEL, ISTECNICO, CPF, EMAIL, SENHA, NOME, TELEFONE, ESTADO, CIDADE, CEP, BAIRRO, RUA,
-COMPLEMENTO) VALUES(4,1,1,'00000000000', 'Maria@hotmail.com', '123456', 'Maria da Massa', '123456789', 'Parana', 'Curitiba', 'TESTE', 'TESTE', 'TESTE','TESTE');
+COMPLEMENTO) VALUES(USUARIOS_SEQ.NEXTVAL,1,1,'00000000000', 'Maria@hotmail.com', '123456', 'Maria da Massa', '123456789', 'MASSSTATE', 'TESTCITY', 'TESTE', 'TESTE', 'TESTE','TESTE');
 
 insert into usuarios(ID_USUARIO, PERMISSIONLEVEL, ISTECNICO, CPF, EMAIL, SENHA, NOME, TELEFONE, ESTADO, CIDADE, CEP, BAIRRO, RUA,
-COMPLEMENTO) VALUES(5,1,1,'00000000000', 'jhonny@hotmail.com', '123456', 'Jhonny test', '123456789', 'Parana', 'Sao Jose', 'TESTE', 'TESTE', 'TESTE','TESTE');
+COMPLEMENTO) VALUES(USUARIOS_SEQ.NEXTVAL,1,1,'00000000000', 'jhonny@hotmail.com', '123456', 'Jhonny test', '123456789', 'TESTSTATE', 'TESTCITY', 'TESTE', 'TESTE', 'TESTE','TESTE');
 
-/
-insert into servicos(id_servico, id_usuario, id_categoria, descricao, preco) values
-	(99, 1, 10, 'Terraplanagem de terros grandes 2', 850.00);
+insert into servicos(id_servico, ID_Tecnico, id_categoria, descricao, preco) values
+	(SERVICOS_SEQ.NEXTVAL, 1, 11, 'Terraplanagem de terros grandes 2', 850.00);
 
-insert into servicos(id_servico, id_usuario, id_categoria, descricao, preco) values
-	(2, 1, 4, 'Concerto de fiacao eletrica', 250.00);
+insert into servicos(id_servico, ID_Tecnico, id_categoria, descricao, preco) values
+	(SERVICOS_SEQ.NEXTVAL, 2, 3, 'Concerto de fiacao eletrica', 250.00);
 
-insert into servicos(id_servico, id_usuario, id_categoria, descricao, preco) values
-	(3, 1, 8, 'Manutencao em piscina', 150.00);
+insert into servicos(id_servico, ID_Tecnico, id_categoria, descricao, preco) values
+	(SERVICOS_SEQ.NEXTVAL, 3, 9, 'Manutencao em piscina', 150.00);
 
-insert into servicos(id_servico, id_usuario, id_categoria, descricao, preco) values
-	(4, 1, 8, 'Limpeza em piscina', 150.00);
+insert into servicos(id_servico, ID_Tecnico, id_categoria, descricao, preco) values
+	(SERVICOS_SEQ.NEXTVAL, 4, 9, 'Limpeza em piscina', 150.00);
 
-insert into servicos(id_servico, id_usuario, id_categoria, descricao, preco) values
-	(5, 1, 8, 'Concerto pscina', 500.00);
+insert into servicos(id_servico, ID_Tecnico, id_categoria, descricao, preco) values
+	(SERVICOS_SEQ.NEXTVAL, 5, 9, 'Concerto piscina', 500.00);
 
-insert into servicos(id_servico, id_usuario, id_categoria, descricao, preco) values
-	(6, 1, 2, 'Detetizacao Basica', 100.00);
+insert into servicos(id_servico, ID_Tecnico, id_categoria, descricao, preco) values
+	(SERVICOS_SEQ.NEXTVAL, 1, 5, 'Detetizacao Basica', 100.00);
 
-insert into servicos(id_servico, id_usuario, id_categoria, descricao, preco) values
-	(7, 1, 7, 'Podagem pequena', 250.00);
+insert into servicos(id_servico, ID_Tecnico, id_categoria, descricao, preco) values
+	(SERVICOS_SEQ.NEXTVAL, 2, 7, 'Podagem pequena', 250.00);
 
-insert into servicos(id_servico, id_usuario, id_categoria, descricao, preco) values
-	(8, 1, 7, 'Podagem Grande', 350.00);
+insert into servicos(id_servico, ID_Tecnico, id_categoria, descricao, preco) values
+	(SERVICOS_SEQ.NEXTVAL, 2, 7, 'Podagem Grande', 350.00);
 
-insert into servicos(id_servico, id_usuario, id_categoria, descricao, preco) values
-	(9, 1, 4, 'Reforma estrutura eletrica', 1500.00);
+insert into servicos(id_servico, ID_Tecnico, id_categoria, descricao, preco) values
+	(SERVICOS_SEQ.NEXTVAL, 4, 3, 'Reforma estrutura eletrica', 1500.00);
 
-insert into servicos(id_servico, id_usuario, id_categoria, descricao, preco) values
-	(10, 1, 10, 'Terraplanagem terros pequenos', 510.00);
+insert into servicos(id_servico, ID_Tecnico, id_categoria, descricao, preco) values
+	(SERVICOS_SEQ.NEXTVAL, 5, 11, 'Terraplanagem terros pequenos', 510.00);
 
-insert into avaliacoes values(3,2,5);
+insert into pedidos (id_pedido, id_usuario_solicitante, id_tecnico, id_servico, data_solicitacao, data_finalizacao, nota)
+    VALUES  (PEDIDOS_SEQ.NEXTVAL,1,2,1, current_timestamp, current_timestamp,null);
+        
+insert into pedidos (id_pedido, id_usuario_solicitante, id_tecnico, id_servico, data_solicitacao, data_finalizacao, nota)
+    VALUES  (PEDIDOS_SEQ.NEXTVAL,1,3,2, current_timestamp, current_timestamp,null);
 
-insert into avaliacoes(id_avaliacao,id_servico,nota) values (1,1,5);
+insert into pedidos (id_pedido, id_usuario_solicitante, id_tecnico, id_servico, data_solicitacao, data_finalizacao, nota)
+    VALUES  (PEDIDOS_SEQ.NEXTVAL,1,2,4, current_timestamp, current_timestamp,null);
 
-insert into pedidos (id_pedido, id_usuario_solicitante, id_servico, id_nota, data_solicitacao, data_finalizacao)
-    VALUES  (3,3,3,3, current_timestamp, current_timestamp);
+insert into pedidos (id_pedido, id_usuario_solicitante, id_tecnico, id_servico, data_solicitacao, data_finalizacao, nota)
+    VALUES  (PEDIDOS_SEQ.NEXTVAL,1,4,3, current_timestamp, current_timestamp,null);
 
+insert into pedidos (id_pedido, id_usuario_solicitante, id_tecnico, id_servico, data_solicitacao, data_finalizacao, nota)
+    VALUES  (PEDIDOS_SEQ.NEXTVAL,1,5,4, current_timestamp, current_timestamp,null);
+    
+insert into pedidos(id_pedido,id_usuario_solicitante,id_tecnico,id_servico,data_solicitacao,data_finalizacao,nota) 
+	VALUES (PEDIDOS_SEQ.NEXTVAL, 1, 2, 1, null, null, null);
+       
 COMMIT;
 
 ------------------------------------PROCEDURES------------------------------------
@@ -251,28 +232,34 @@ END CAT_MAIOR_NUMERO_SERVICOS;
 
 --exec CAT_MAIOR_NUMERO_SERVICOS;
 
-CREATE OR REPLACE PROCEDURE ATUALIZA_SERVICOS_FINALIZADOS
+CREATE OR REPLACE PROCEDURE VERIFICA_SERVICO_FINALIZADO (pID_PEDIDO NUMBER)
 IS
     var_ID_PEDIDO NUMBER(38,0);
-	var_ID_USUARIO_SOLICITANTE NUMBER(38,0);
-    var_ID_SERVICO NUMBER(38,0);
-    var_ID_NOTA NUMBER(38,0);
-    var_DATA_SOLICITACAO TIMESTAMP;
     var_DATA_FINALIZACAO TIMESTAMP;      
 BEGIN
-	SELECT * INTO var_ID_PEDIDO, var_ID_USUARIO_SOLICITANTE, var_ID_SERVICO, var_ID_NOTA, 
-		var_DATA_SOLICITACAO, var_DATA_FINALIZACAO FROM PEDIDOS WHERE ID_PEDIDO = 3;
-		
-		IF var_DATA_FINALIZACAO is not null  
-			THEN        
-				DBMS_OUTPUT.PUT_LINE('Executando insert');
-				insert into Pedidos_Finalizados(ID_PEDIDO_FINALIZADO, ID_SERVICO, ID_PEDIDO,
-					ID_CATEGORIA, ID_NOTA, DATAFINALIZACAO, ID_USUARIO_CONTRATANTE, ID_TECNICO_FORNECEDOR)
-				VALUES (SERVICOS_FINALIZADOS_SEQ.NEXTVAL, var_ID_SERVICO, var_ID_PEDIDO, 1, var_ID_NOTA, var_DATA_FINALIZACAO, var_ID_USUARIO_SOLICITANTE, 1);            
-		END IF;
-		DBMS_OUTPUT.PUT_LINE('Servico :' ||var_ID_PEDIDO|| ' atualizado na base de dados com a data ' ||var_DATA_FINALIZACAO|| ' .');
-END ATUALIZA_SERVICOS_FINALIZADOS;
+	SELECT ID_PEDIDO, DATA_FINALIZACAO INTO var_ID_PEDIDO, var_DATA_FINALIZACAO FROM PEDIDOS WHERE ID_PEDIDO = pID_PEDIDO;    
+     if var_DATA_FINALIZACAO is not null
+       then
+            DBMS_OUTPUT.PUT_LINE('O pedido ' ||var_ID_PEDIDO|| ' ja foi finalizado.');
+            DBMS_OUTPUT.PUT_LINE('Data da finalizacao: ' ||var_DATA_FINALIZACAO|| ' .');     
+    else
+        DBMS_OUTPUT.PUT_LINE('O pedido ' ||var_ID_PEDIDO|| ' ainda nao foi finalizado.');    
+    END IF;              
+END VERIFICA_SERVICO_FINALIZADO;
 /
-
---exec ATUALIZA_SERVICOS_FINALIZADOS;
+--exec verifica_servico_finalizado(5)
+--exec verifica_servico_finalizado(6)
 ------------------------------------END PROCEDURES------------------------------------
+
+------------------------------------SCRIPT ROLLBACK------------------------------------
+--drop table pedidos;
+--drop table servicos;
+--drop table categorias_Servicos;
+--drop table enderecos_Secundarios;
+--drop table usuarios;
+--commit;
+--disconnect
+--connect system/system
+--drop user iService cascade;
+--disconnect
+------------------------------------SCRIPT ROLLBACK------------------------------------
